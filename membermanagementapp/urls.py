548a595d8895953fe2c56334b36fb09391e5c830
gavindang2911 <hoaidang2999@gmail.com>
2021-12-09 00:58:58 -0800

    working on addItem view

--- membermanagementapp/urls.py
+++ membermanagementapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='membermangement-index')
+    path('', views.index, name='membermangement-index'),
+    path('addItem', views.add_item, name='membermangement-add-item')
 ]
