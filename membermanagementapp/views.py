e3acf31cf9ec6b1f63798037c61ee7de1a498a6c
gavindang2911 <hoaidang2999@gmail.com>
2021-12-09 02:24:53 -0800

    Displayed to the main page

--- membermanagementapp/views.py
+++ membermanagementapp/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Member
 
 # Create your views here.
 def index(request):
-    return render(request, 'membermanagementapp/index.html')
+    members = Member.objects.all()
+
+    context = {
+        'members': members
+    }
+    return render(request, 'membermanagementapp/index.html', context)
 
 def add_item(request):
     return render(request, 'membermanagementapp/addItem.html')
