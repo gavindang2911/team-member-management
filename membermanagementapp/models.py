d717a0da4b6e03aa214867cc2d590140c94412ab
gavindang2911 <hoaidang2999@gmail.com>
2021-12-09 02:18:05 -0800

    Added Member model

--- membermanagementapp/models.py
+++ membermanagementapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    first_name  = models.CharField(max_length=50)
+    last_name   = models.CharField(max_length=50)
+    email  = models.EmailField(max_length=70,blank=True)
+    phone_number  = models.CharField(max_length=50,blank=True,null=True)
+    admin_role = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.first_name
