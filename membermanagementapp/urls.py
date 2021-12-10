e64ef970f1b9d30076db04b493e29cf71e9b9da7
gavindang2911 <hoaidang2999@gmail.com>
2021-12-10 01:21:14 -0800

    Updated delete function

--- membermanagementapp/urls.py
+++ membermanagementapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='membermangement-index'),
     path('addMember', views.add_item, name='membermangement-add-item'),
-    path('edit/<int:pk>/', views.edit, name='membermangement-edit-item')
+    path('edit/<int:pk>/', views.edit, name='membermangement-edit-item'),
+    path('delete/<int:pk>/', views.delete, name='membermangement-delete-item')
 ]
--- membermanagementapp/views.py
+++ membermanagementapp/views.py
@@ -7,8 +7,10 @@ from .forms import MemberForm
 def index(request):
     # members = Member.objects.all()
     members =  Member.objects.all().order_by('role')
-    count_members = Member.objects.filter(role='Regular Member').count()
-    count_admin = Member.objects.filter(role='Admin').count()
+    # count_members = Member.objects.filter(role='Regular Member').count()
+    # count_admin = Member.objects.filter(role='Admin').count()
+
+    count_members, count_admin = member_count_helper()
 
     context = {
         'members': members,
@@ -17,18 +19,6 @@ def index(request):
     }
     return render(request, 'membermanagementapp/index.html', context)
 
-# def add_item(request):
-#     return render(request, 'membermanagementapp/addItem.html')
-
-# def add_item(request):
-# 	form = MemberForm(request.POST or None)
-# 	if form.is_valid():
-# 		form.save()
-#         # return redirect('/')
-# 	context = {
-# 		"form": form,
-# 	}
-# 	return render(request, "membermanagementapp/addItem.html", context)
 def add_item(request):
     if request.method == 'POST':
         form = MemberForm(request.POST)
@@ -38,8 +28,10 @@ def add_item(request):
     else:
         form = MemberForm()
 
-    count_members = Member.objects.filter(role='Regular Member').count()
-    count_admin = Member.objects.filter(role='Admin').count()
+    # count_members = Member.objects.filter(role='Regular Member').count()
+    # count_admin = Member.objects.filter(role='Admin').count()
+    count_members, count_admin = member_count_helper()
+
 
     context = {
         'form': form,
@@ -49,10 +41,11 @@ def add_item(request):
     return render(request, "membermanagementapp/addMember.html", context)
 
 def edit(request, pk):
-    # members = Member.objects.all()
     member_by_id =  Member.objects.get(id=pk)
-    count_members = Member.objects.filter(role='Regular Member').count()
-    count_admin = Member.objects.filter(role='Admin').count()
+    # count_members = Member.objects.filter(role='Regular Member').count()
+    # count_admin = Member.objects.filter(role='Admin').count()
+    count_members, count_admin = member_count_helper()
+
 
     if request.method == 'POST':
         form = MemberForm(request.POST, instance=member_by_id)
@@ -64,6 +57,29 @@ def edit(request, pk):
     context = {
         'form': form,
         'count_members': count_members,
-        'count_admin': count_admin
+        'count_admin': count_admin,
+        'member_by_id': member_by_id
     }
     return render(request, 'membermanagementapp/editMember.html', context)
+
+def delete(request, pk):
+    member_by_id =  Member.objects.get(id=pk)
+    count_members, count_admin = member_count_helper()
+
+    if request.method == 'POST':
+        member_by_id.delete()
+        return redirect('/')
+
+    context = {
+        'count_members': count_members,
+        'count_admin': count_admin
+    }
+
+    return render(request, 'membermanagementapp/deleteMember.html', context)
+
+
+def member_count_helper():
+    count_members = Member.objects.filter(role='Regular Member').count()
+    count_admin = Member.objects.filter(role='Admin').count()
+
+    return count_members, count_admin
