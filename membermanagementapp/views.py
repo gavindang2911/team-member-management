46d464e46eee8917ed0a14ded80ffea303e7b164
gavindang2911 <hoaidang2999@gmail.com>
2021-12-09 23:26:22 -0800

    Display the total member

--- membermanagementapp/views.py
+++ membermanagementapp/views.py
@@ -5,10 +5,15 @@ from .forms import MemberForm
 
 # Create your views here.
 def index(request):
-    members = Member.objects.all()
+    # members = Member.objects.all()
+    members =  Member.objects.all().order_by('role')
+    count_members = members.count()
+    count_admin = Member.objects.filter(role='Admin').count()
 
     context = {
-        'members': members
+        'members': members,
+        'count_members': count_members,
+        'count_admin': count_admin
     }
     return render(request, 'membermanagementapp/index.html', context)
 
@@ -25,14 +30,21 @@ def index(request):
 # 	}
 # 	return render(request, "membermanagementapp/addItem.html", context)
 def add_item(request):
-	if request.method == 'POST':
-		form = MemberForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return redirect('/')
-	else:
-		form = MemberForm()
-	context = {
-		'form': form
-	}
-	return render(request, "membermanagementapp/addItem.html", context)
+    if request.method == 'POST':
+        form = MemberForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+    else:
+        form = MemberForm()
+
+    members =  Member.objects.all().order_by('role')
+    count_members = members.count()
+    count_admin = Member.objects.filter(role='Admin').count()
+
+    context = {
+        'form': form,
+        'count_members': count_members,
+        'count_admin': count_admin
+    }
+    return render(request, "membermanagementapp/addItem.html", context)
