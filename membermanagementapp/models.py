c40c126c3a171a3bb09b4871c8b7923b5c88b874
gavindang2911 <hoaidang2999@gmail.com>
2021-12-09 22:31:45 -0800

    Fixed Member model, added role choice field

--- membermanagementapp/models.py
+++ membermanagementapp/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
+role_choice = (
+		('Admin', 'Admin'),
+		('Regular Member', 'Regular Member'),
+)
 # Create your models here.
 class Member(models.Model):
     first_name  = models.CharField(max_length=50)
     last_name   = models.CharField(max_length=50)
     email  = models.EmailField(max_length=70,blank=True)
     phone_number  = models.CharField(max_length=50,blank=True,null=True)
-    admin_role = models.BooleanField(default=False)
+    role = models.CharField(max_length=50, blank=True, null=True, choices=role_choice, default='Regular Member')
 
     def __str__(self):
         return self.first_name
